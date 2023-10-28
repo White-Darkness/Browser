ca261c7a63a46e7621f1fbc63cadf70999cbd317
hkr <149226162+White-Darkness@users.noreply.github.com>
2023-10-28 21:47:57 +0530

    Tool-url.py

--- Tool-url.py
+++ Tool-url.py
@@ -1,1 +1,8 @@
+from googlesearch import search
+print("If you having any doubt then run README.md file")
+data = input("Search here : - ")
 
+for i in search(data, 50, advanced=True):
+     print(i.url)
+     print()
+     print()
