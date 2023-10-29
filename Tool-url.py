c5a2f1b2020d3b56216b3d2d67bf70a44ba00dc2
hkr <149226162+White-Darkness@users.noreply.github.com>
2023-10-29 10:02:05 +0530

    Tool-url.py

--- Tool-url.py
+++ Tool-url.py
@@ -2,6 +2,8 @@ from googlesearch import search
 print()
 print("Instagram >> cyber_d_hkr")
 print()
+print("If you want to stop execution the press ctrl + z")
+print()
 print("If you having any doubt then run README.md file")
 print()
 data = input("Search here : - ")
